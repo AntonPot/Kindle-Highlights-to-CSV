61ce1befebfd31a79c4c957e09532d74c77e9134
Marko Anton Potocnik <markoapotocnik@hotmail.com>
2016-06-24 15:37:01 +0200

    downloading only hash

--- highlights.rb
+++ highlights.rb
@@ -1,22 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 require 'shotgun'
-require 'dotenv'
 
 require_relative './parser.rb'
-
-Dotenv.load
 
 get '/' do
   erb :index
 end
 
 post '/form' do
-  filename = 'kindle_highlights.csv'
-  email = params[:email]
-  password = params[:password]
-  data = Parser::Kindle.new(email, password).books_highlights
-  Parser::CsvGenerator.go( data, filename )
+  # filename = 'kindle_highlights.csv'
+  filename = 'kindle_highlights_hash'
+  data = Parser::Kindle.new(params[:email], params[:password]).books_highlights
+  p data
+  File.open(filename, "w") { |file| file.write(data) }
+  # Parser::CsvGenerator.go( data, filename )
   redirect "/download/#{filename}"
 end
 
